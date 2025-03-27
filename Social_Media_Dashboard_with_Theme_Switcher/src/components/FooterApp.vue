34017e69ab7c2d8d7953cf05e23d02fc116df44c
Dmytro Zaiets <dmytrozaiets81@gmail.com>
2025-03-27 11:26:38 -0400

    Foldername

---
+++ Social_Media_Dashboard_with_Theme_Switcher/src/components/FooterApp.vue
@@ -0,0 +1,35 @@
+<template>
+	<footer class="attribution">
+		Challenge by
+		<a
+			href="https://github.com/Dmytrozaiets81"
+			target="_blank"
+			>Frontend Mentor</a
+		>
+		Coded by
+		<a
+			href="https://github.com/Dmytrozaiets81/Mentor-Frontend/"
+			target="_blank"
+			>Dmytro Zaiets</a
+		>
+	</footer>
+</template>
+
+<style lang="scss" scoped>
+	.attribution {
+		font-size: 1rem;
+		text-align: center;
+		color: var(--big-text);
+		margin-bottom: 3rem;
+
+		a {
+			color: var(--twitter);
+
+			&:hover {
+				text-decoration: underline;
+			}
+
+			@extend %link-focus;
+		}
+	}
+</style>
